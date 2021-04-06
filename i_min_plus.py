9c4d77e97c69bc6a6465f3ab9226a8a462037c12
ysr075 <74384666+ysr075@users.noreply.github.com>
2021-04-06 19:13:58 +0200

    i_min_plus.py

--- i_min_plus.py
+++ i_min_plus.py
@@ -1,1 +1,27 @@
+from time import sleep
 
+def i_plus_plus():
+	i = 0
+	while True:
+		i += 1
+		print("case " + str(i))
+		sleep(0.5)
+
+def i_min_min():
+	i = 0
+	while True:
+		i -= 1
+		print("case " + str(i))
+		sleep(0.5)
+
+def main():
+	choice = input("plus / min: ")
+	if choice == "plus":
+		i_plus_plus()
+	elif choice	== "min":
+		i_min_min()
+	else:
+		return main()
+
+if __name__ == '__main__':
+	main()
